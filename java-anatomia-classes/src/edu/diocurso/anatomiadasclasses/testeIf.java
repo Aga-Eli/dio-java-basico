e4fc9a3918e23503bfcbb0503a14fc0d80f3d046
Aga-Eli <matheuslegw@gmail.com>
2024-05-29 00:24:20 -0300

    Commit-Anatomia-Classes

---
+++ java-anatomia-classes/src/edu/diocurso/anatomiadasclasses/testeIf.java
@@ -0,0 +1,11 @@
+package edu.diocurso.anatomiadasclasses;
+
+public class testeIf {
+    public static void main(String[] args) {
+        int num = 6;
+        if (num != 6){
+            System.err.println("é 6");
+            System.err.println("passou");
+        }
+    }
+}
