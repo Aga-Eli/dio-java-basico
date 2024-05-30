26e847fe69d124a7b2ce6e6ba49511680fa777d2
Aga-Eli <matheuslegw@gmail.com>
2024-05-30 02:32:30 -0300

    Adiciona Aula Palavras Reservadas e alguns exemplos

---
+++ Java-Palavras Reservadas/src/palavrasReservadas.java
@@ -0,0 +1,31 @@
+public class palavrasReservadas {
+    public static void main(String[] args) throws Exception {
+        // ao todo, 52:
+
+        //Controle de pacotes:
+        // import: importa pacotes ou classes para dentro do código
+        // package: especifica a que pacote todas as classes de um arquivo pertencem
+        
+        // Modificadore de acesso:
+        // public: acesso de qualquer classe
+        // private acesso apenas dentro da classe
+        // protected: acesso por classes no mesmo pacote e subclasses
+
+        // Primitivos:
+        // As classicas int, char, byte, float,..., void
+        
+        // Modificadores de classes, variáveis ou métodos:
+        // abstract: clase que não poderia ser intanciada ou método que precisar ser implementado por uma subclasse não abstrata
+        // class: especifica uma classe
+        // extends: indica a superclasse que a subclasse está estendendo
+        // final
+        // implements: indica as interfaces que uma classe irá implementar
+        // interface: especifica uma interface
+        // native: indica que um método está escrito em uma linguagem dependente de plataforma, como o C
+        // new
+        // statis: faz um método ou variável petencer à classe ao invés de às instâncias
+
+        //...
+
+    }
+}
