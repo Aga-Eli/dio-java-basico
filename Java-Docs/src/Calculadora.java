225fc8abccd3075b1d6ff561cbf85f38d6a61d1b
Aga-Eli <matheuslegw@gmail.com>
2024-05-30 13:04:20 -0300

    Adiciona a Aula Java Docs e a pasta de documntação gerada

---
+++ Java-Docs/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números
+ * <p>
+ * <b>Note:</b> Leia atentamentea documentação desta classe
+ * para desfrutar dos recursos oferecidospelo autor
+ * 
+ * @author Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste é a soma dos dois números
+     */
+    public static int somar (int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+    
+}
---
+++ Java-Docs/src/Comentários.java
@@ -0,0 +1,29 @@
+public class Comentários {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+
+     // Experimente passar o mouse do metodo abaixo dentro de uma ide
+
+    public void metodo(){
+
+    }
+
+    /* use "javadoc-encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java"
+    * no terminal para criar uma pasta "docs" que cria a documentação do seu programa
+    */
+}
